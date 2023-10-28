b8dc087b8def3bbc00bae8f8f9fe603753f8dd4e
xrack1023 <jkparrett10@gmail.com>
2023-10-27 20:31:56 -0600

    Final, Submitted

--- AntivirusAssignment/antivirusAssignment.cpp
+++ AntivirusAssignment/antivirusAssignment.cpp
@@ -8,26 +8,23 @@ int str_length(char str[]) {
 }
 
 int main() {
-	int DNA, i= 0, countG = 0;
-	char str[100], l = 23, initial;
+	int i= 0, countG = 0;
+	char str[100], l = 23;
 
 	printf("Enter the genome: ");
 	scanf("%s", str);
 
-	//need to make this either NEGATIVE or POSITIVE if it contains exactly 7 g's
 	while (str[i] != '\0') {
 		if (str[i] == 'g' || str[i] == 'G') {
 			countG++;
 		}
 		i++;
-	}
-	
-	//printf("G: %d\n", countG);
+	}	
 
 	int length = str_length(str);
 
 	if (str_length(str) != l){
-		printf("NEGATIVE: length is not 23\n");
+		printf("NEGATIVE: Length is not 23\n");
 	}
 	else {
 		printf("Length is 23\n");
@@ -36,24 +33,25 @@ int main() {
 			printf("First letter is 'a'\n");
 		}
 		else {
-			printf("Negative: first letter is not 'a'\n");
+			printf("Negative: First letter is not 'a'\n");
 			return 0;
 		}
 		if (str[22] == 't') {
 			printf("Last letter is 't'\n");
 		}
 		else {
-			printf("Negative: last letter is not 't'\n");
+			printf("Negative: Last letter is not 't'\n");
 			return 0;
 		}
 		if (countG == 7) {
-			printf("Contains 7 'g's'");
+			printf("Contains exactly 7 'g's'\n");
 		}
 		else {
-			printf("does not contain 7 g's");
+			printf("NEGATIVE: Does not contain exactly 7 g's\n");
 			return 0;
 		}
+	printf("POSITIVE: all conditions met\n");
 	}
-	printf("Positive: all conditions met");
+	
 	return 0;
 }
