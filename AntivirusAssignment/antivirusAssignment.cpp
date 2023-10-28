1f7f060136ec098014c6c5948d43734032bdc6ec
xrack1023 <jkparrett10@gmail.com>
2023-10-27 19:36:37 -0600

    working code

--- AntivirusAssignment/antivirusAssignment.cpp
+++ AntivirusAssignment/antivirusAssignment.cpp
@@ -9,7 +9,7 @@ int str_length(char str[]) {
 
 int main() {
 	int DNA, i= 0, countG = 0;
-	char str[100], l = 23;
+	char str[100], l = 23, initial;
 
 	printf("Enter the genome: ");
 	scanf("%s", str);
@@ -21,17 +21,39 @@ int main() {
 		}
 		i++;
 	}
-
-	printf("G: %d\n", countG);
+	
+	//printf("G: %d\n", countG);
 
 	int length = str_length(str);
 
-	if (str_length(str) == l){
-		printf("POSITIVE: length is 23");
+	if (str_length(str) != l){
+		printf("NEGATIVE: length is not 23\n");
 	}
 	else {
-		printf("NEGATIVE: length is not 23");
-	}
+		printf("Length is 23\n");
 
+		if (str[0] == 'a') {
+			printf("First letter is 'a'\n");
+		}
+		else {
+			printf("Negative: first letter is not 'a'\n");
+			return 0;
+		}
+		if (str[22] == 't') {
+			printf("Last letter is 't'\n");
+		}
+		else {
+			printf("Negative: last letter is not 't'\n");
+			return 0;
+		}
+		if (countG == 7) {
+			printf("Contains 7 'g's'");
+		}
+		else {
+			printf("does not contain 7 g's");
+			return 0;
+		}
+	}
+	printf("Positive: all conditions met");
 	return 0;
 }
